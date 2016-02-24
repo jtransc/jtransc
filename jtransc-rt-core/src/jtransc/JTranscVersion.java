f34e9f72d1339e76efeba5c070135f93f47c9334
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-02-24 15:28:13 +0100

    Small fix

--- jtransc-rt-core/src/jtransc/JTranscVersion.java
+++ jtransc-rt-core/src/jtransc/JTranscVersion.java
@@ -1,9 +1,13 @@
 package jtransc;
 
+import jtransc.annotation.haxe.HaxeMethodBody;
+
 public class JTranscVersion {
 	static public String getVersion() {
 		return "0.1.0";
 	}
+
+	@HaxeMethodBody("return HaxeNatives.str('haxe');")
 	static public String getRuntime() {
 		return "java";
 	}
