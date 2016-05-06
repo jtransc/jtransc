6f951d73c85e2c9a1e65e3050ddc113b05541c1e
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-05-06 11:53:49 +0200

    Updated version to 0.2.2

--- jtransc-rt-core/src/com/jtransc/JTranscVersion.java
+++ jtransc-rt-core/src/com/jtransc/JTranscVersion.java
@@ -3,7 +3,7 @@ package com.jtransc;
 import com.jtransc.annotation.haxe.HaxeMethodBody;
 
 public class JTranscVersion {
-	static private final String version = "0.2.1";
+	static private final String version = "0.2.2";
 
 	static public String getVersion() {
 		return version;
