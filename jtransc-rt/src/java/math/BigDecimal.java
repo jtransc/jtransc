d902c858ce0dba461b5c96498b24acd60d28f420
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-06-20 14:28:23 +0200

    Added dummy BigDecimal and BigInteger

--- jtransc-rt/src/java/math/BigDecimal.java
+++ jtransc-rt/src/java/math/BigDecimal.java
@@ -1,12 +1,236 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package java.math;
 
-public class BigDecimal {
-	public final static int ROUND_UP = 0;
-	public final static int ROUND_DOWN = 1;
-	public final static int ROUND_CEILING = 2;
-	public final static int ROUND_FLOOR = 3;
-	public final static int ROUND_HALF_UP = 4;
-	public final static int ROUND_HALF_DOWN = 5;
-	public final static int ROUND_HALF_EVEN = 6;
-	public final static int ROUND_UNNECESSARY = 7;
+import java.io.Serializable;
+
+public class BigDecimal extends Number implements Comparable<BigDecimal>, Serializable {
+	public static final int ROUND_UP = 0;
+	public static final int ROUND_DOWN = 1;
+	public static final int ROUND_CEILING = 2;
+	public static final int ROUND_FLOOR = 3;
+	public static final int ROUND_HALF_UP = 4;
+	public static final int ROUND_HALF_DOWN = 5;
+	public static final int ROUND_HALF_EVEN = 6;
+	public static final int ROUND_UNNECESSARY = 7;
+	public static final BigDecimal ZERO = new BigDecimal(0, 0);
+	public static final BigDecimal ONE = new BigDecimal(1, 0);
+	public static final BigDecimal TEN = new BigDecimal(10, 0);
+
+	private BigDecimal(int smallValue, int scale) {
+
+	}
+
+	private BigDecimal(long smallValue, int scale) {
+
+	}
+
+	public BigDecimal(char[] in, int offset, int len) {
+	}
+
+	public BigDecimal(char[] in, int offset, int len, MathContext mc) {
+		this(in, offset, len);
+		//inplaceRound(mc);
+	}
+
+	public BigDecimal(char[] in) {
+		this(in, 0, in.length);
+	}
+
+	public BigDecimal(char[] in, MathContext mc) {
+		this(in, 0, in.length);
+		//inplaceRound(mc);
+	}
+
+	public BigDecimal(String val) {
+		this(val.toCharArray(), 0, val.length());
+	}
+
+	public BigDecimal(String val, MathContext mc) {
+		this(val.toCharArray(), 0, val.length());
+		//inplaceRound(mc);
+	}
+
+	public BigDecimal(double val) {
+	}
+
+	public BigDecimal(double val, MathContext mc) {
+		this(val);
+		//inplaceRound(mc);
+	}
+
+	public BigDecimal(BigInteger val) {
+		this(val, 0);
+	}
+
+	public BigDecimal(BigInteger val, MathContext mc) {
+		this(val);
+		//inplaceRound(mc);
+	}
+
+	public BigDecimal(BigInteger unscaledVal, int scale) {
+		if (unscaledVal == null) {
+			throw new NullPointerException("unscaledVal == null");
+		}
+		//this.scale = scale;
+		//setUnscaledValue(unscaledVal);
+	}
+
+	public BigDecimal(BigInteger unscaledVal, int scale, MathContext mc) {
+		this(unscaledVal, scale);
+		//inplaceRound(mc);
+	}
+
+	public BigDecimal(int val) {
+		this(val, 0);
+	}
+
+	public BigDecimal(int val, MathContext mc) {
+		this(val, 0);
+		//inplaceRound(mc);
+	}
+
+	public BigDecimal(long val) {
+		this(val, 0);
+	}
+
+	public BigDecimal(long val, MathContext mc) {
+		this(val);
+		//inplaceRound(mc);
+	}
+
+	native public static BigDecimal valueOf(long unscaledVal, int scale);
+
+	native public static BigDecimal valueOf(long unscaledVal);
+
+	native public static BigDecimal valueOf(double val);
+
+	native public BigDecimal add(BigDecimal augend);
+
+	native public BigDecimal add(BigDecimal augend, MathContext mc);
+
+	native public BigDecimal subtract(BigDecimal subtrahend);
+
+	native public BigDecimal subtract(BigDecimal subtrahend, MathContext mc);
+
+	native public BigDecimal multiply(BigDecimal multiplicand);
+
+	native public BigDecimal multiply(BigDecimal multiplicand, MathContext mc);
+
+	native public BigDecimal divide(BigDecimal divisor, int scale, int roundingMode);
+
+	native public BigDecimal divide(BigDecimal divisor, int scale, RoundingMode roundingMode);
+
+	native public BigDecimal divide(BigDecimal divisor, int roundingMode);
+
+	native public BigDecimal divide(BigDecimal divisor, RoundingMode roundingMode);
+
+	native public BigDecimal divide(BigDecimal divisor);
+
+	native public BigDecimal divide(BigDecimal divisor, MathContext mc);
+
+	native public BigDecimal divideToIntegralValue(BigDecimal divisor);
+
+	native public BigDecimal divideToIntegralValue(BigDecimal divisor, MathContext mc);
+
+	native public BigDecimal remainder(BigDecimal divisor);
+
+	native public BigDecimal remainder(BigDecimal divisor, MathContext mc);
+
+	native public BigDecimal[] divideAndRemainder(BigDecimal divisor);
+
+	native public BigDecimal[] divideAndRemainder(BigDecimal divisor, MathContext mc);
+
+	native public BigDecimal pow(int n);
+
+	native public BigDecimal pow(int n, MathContext mc);
+
+	native public BigDecimal abs();
+
+	native public BigDecimal abs(MathContext mc);
+
+	native public BigDecimal negate();
+
+	native public BigDecimal negate(MathContext mc);
+
+	native public BigDecimal plus();
+
+	native public BigDecimal plus(MathContext mc);
+
+	native public int signum();
+
+	native public int scale();
+
+	native public int precision();
+
+	native public BigInteger unscaledValue();
+
+	native public BigDecimal round(MathContext mc);
+
+	native public BigDecimal setScale(int newScale, RoundingMode roundingMode);
+
+	native public BigDecimal setScale(int newScale, int roundingMode);
+
+	native public BigDecimal setScale(int newScale);
+
+	native public BigDecimal movePointLeft(int n);
+
+	native public BigDecimal movePointRight(int n);
+
+	native public BigDecimal scaleByPowerOfTen(int n);
+
+	native public BigDecimal stripTrailingZeros();
+
+	native public int compareTo(BigDecimal val);
+
+	native public boolean equals(Object x);
+
+	native public BigDecimal min(BigDecimal val);
+
+	native public BigDecimal max(BigDecimal val);
+
+	native public int hashCode();
+
+	native public String toString();
+
+	native public String toEngineeringString();
+
+	native public String toPlainString();
+
+	native public BigInteger toBigInteger();
+
+	native public BigInteger toBigIntegerExact();
+
+	native public long longValue();
+
+	native public long longValueExact();
+
+	native public int intValue();
+
+	native public int intValueExact();
+
+	native public short shortValueExact();
+
+	native public byte byteValueExact();
+
+	native public float floatValue();
+
+	native public double doubleValue();
+
+	native public BigDecimal ulp();
 }
--- jtransc-rt/src/java/math/BigInteger.java
+++ jtransc-rt/src/java/math/BigInteger.java
@@ -1,100 +1,74 @@
 /*
- * Copyright 2016 Carlos Ballesteros Velasco
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package java.math;
 
+import java.io.Serializable;
 import java.util.Random;
 
-public class BigInteger extends Number implements Comparable<BigInteger> {
-	public BigInteger(byte[] val) {
+public class BigInteger extends Number implements Comparable<BigInteger>, Serializable {
+	public static final BigInteger ZERO = new BigInteger(0, 0);
+	public static final BigInteger ONE = new BigInteger(1, 1);
+	public static final BigInteger TEN = new BigInteger(1, 10);
+
+	BigInteger(int sign, long value) {
+	}
+
+	BigInteger(int sign, int numberLength, int[] digits) {
+		//setJavaRepresentation(sign, numberLength, digits);
+	}
+
+	public BigInteger(int numBits, Random random) {
+	}
+
+	public BigInteger(int bitLength, int certainty, Random random) {
+	}
+
+	public BigInteger(String value) {
+	}
+
+	public BigInteger(String value, int radix) {
 	}
 
 	public BigInteger(int signum, byte[] magnitude) {
 	}
 
-	public BigInteger(String val, int radix) {
+	public BigInteger(byte[] value) {
 	}
 
-	BigInteger(char[] val, int sign, int len) {
-	}
+	native public static BigInteger valueOf(long value);
 
-	public BigInteger(String val) {
-		this(val, 10);
-	}
-
-	public BigInteger(int numBits, Random rnd) {
-
-	}
-
-	public BigInteger(int bitLength, int certainty, Random rnd) {
-	}
-
-	native public static BigInteger probablePrime(int bitLength, Random rnd);
-
-	native public BigInteger nextProbablePrime();
-
-	native public static BigInteger valueOf(long val);
-
-	public static final BigInteger ZERO = valueOf(0);
-	public static final BigInteger ONE = valueOf(1);
-	//private static final BigInteger TWO = valueOf(2);
-	//private static final BigInteger NEGATIVE_ONE = valueOf(-1);
-	public static final BigInteger TEN = valueOf(10);
-
-	native public BigInteger add(BigInteger val);
-
-	native public BigInteger subtract(BigInteger val);
-
-	native public BigInteger multiply(BigInteger val);
-
-	native public BigInteger divide(BigInteger val);
-
-	native public BigInteger[] divideAndRemainder(BigInteger val);
-
-	native public BigInteger remainder(BigInteger val);
-
-	native public BigInteger pow(int exponent);
-
-	native public BigInteger gcd(BigInteger val);
+	native public byte[] toByteArray();
 
 	native public BigInteger abs();
 
 	native public BigInteger negate();
 
+	native public BigInteger add(BigInteger value);
+
+	native public BigInteger subtract(BigInteger value);
+
 	native public int signum();
-
-	native public BigInteger mod(BigInteger m);
-
-	native public BigInteger modPow(BigInteger exponent, BigInteger m);
-
-	native public BigInteger modInverse(BigInteger m);
-
-	native public BigInteger shiftLeft(int n);
 
 	native public BigInteger shiftRight(int n);
 
-	native public BigInteger and(BigInteger val);
+	native public BigInteger shiftLeft(int n);
 
-	native public BigInteger or(BigInteger val);
-
-	native public BigInteger xor(BigInteger val);
-
-	native public BigInteger not();
-
-	native public BigInteger andNot(BigInteger val);
+	native public int bitLength();
 
 	native public boolean testBit(int n);
 
@@ -106,27 +80,17 @@ public class BigInteger extends Number implements Comparable<BigInteger> {
 
 	native public int getLowestSetBit();
 
-	native public int bitLength();
-
 	native public int bitCount();
 
-	native public boolean isProbablePrime(int certainty);
+	native public BigInteger not();
 
-	native public int compareTo(BigInteger val);
+	native public BigInteger and(BigInteger value);
 
-	native public boolean equals(Object x);
+	native public BigInteger or(BigInteger value);
 
-	native public BigInteger min(BigInteger val);
+	native public BigInteger xor(BigInteger value);
 
-	native public BigInteger max(BigInteger val);
-
-	native public int hashCode();
-
-	native public String toString(int radix);
-
-	native public String toString();
-
-	native public byte[] toByteArray();
+	native public BigInteger andNot(BigInteger value);
 
 	native public int intValue();
 
@@ -135,4 +99,42 @@ public class BigInteger extends Number implements Comparable<BigInteger> {
 	native public float floatValue();
 
 	native public double doubleValue();
+
+	native public int compareTo(BigInteger value);
+
+	native public BigInteger min(BigInteger value);
+
+	native public BigInteger max(BigInteger value);
+
+	native public int hashCode();
+
+	native public boolean equals(Object x);
+
+	native public String toString();
+
+	native public String toString(int radix);
+
+	native public BigInteger gcd(BigInteger value);
+
+	native public BigInteger multiply(BigInteger value);
+
+	native public BigInteger pow(int exp);
+
+	native public BigInteger[] divideAndRemainder(BigInteger divisor);
+
+	native public BigInteger divide(BigInteger divisor);
+
+	native public BigInteger remainder(BigInteger divisor);
+
+	native public BigInteger modInverse(BigInteger m);
+
+	native public BigInteger modPow(BigInteger exponent, BigInteger modulus);
+
+	native public BigInteger mod(BigInteger m);
+
+	native public boolean isProbablePrime(int certainty);
+
+	native public BigInteger nextProbablePrime();
+
+	native public static BigInteger probablePrime(int bitLength, Random random);
 }
