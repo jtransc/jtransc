9ee435f4c6e60dc7bdd4b251a3df9e9fc8345818
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-06-30 20:12:06 +0200

    Update kotlin to 1.0.3 Try to fix travis

--- jtransc-utils/test/LocateKotlinTest.kt
+++ jtransc-utils/test/LocateKotlinTest.kt
@@ -1,3 +1,4 @@
+import com.jtransc.KotlinVersion
 import com.jtransc.maven.GradleLocalRepository
 import com.jtransc.maven.MavenGradleLocalRepository
 import org.junit.Assert
@@ -6,7 +7,7 @@ import org.junit.Test
 class LocateKotlinTest {
 	@Test
 	fun name() {
-		Assert.assertEquals(1, GradleLocalRepository.locateJars("org.jetbrains.kotlin:kotlin-runtime:1.0.2-1").size)
-		Assert.assertEquals(true, MavenGradleLocalRepository.locateJars("org.jetbrains.kotlin:kotlin-runtime:1.0.2-1").size >= 1)
+		Assert.assertEquals(1, GradleLocalRepository.locateJars("org.jetbrains.kotlin:kotlin-runtime:$KotlinVersion").size)
+		Assert.assertEquals(true, MavenGradleLocalRepository.locateJars("org.jetbrains.kotlin:kotlin-runtime:$KotlinVersion").size >= 1)
 	}
 }
