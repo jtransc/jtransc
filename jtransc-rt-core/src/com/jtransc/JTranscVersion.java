3324e6fa2f3e8836b6c6d5adc82d804d661efcfc
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-12-12 22:37:37 +0100

    Updated to 0.5.2 supporting limiting exploration with @JtranscMethodBody annotations

--- jtransc-rt-core/src/com/jtransc/JTranscVersion.java
+++ jtransc-rt-core/src/com/jtransc/JTranscVersion.java
@@ -1,7 +1,7 @@
 package com.jtransc;
 
 public class JTranscVersion {
-	static private final String version = "0.5.1";
+	static private final String version = "0.5.2";
 
 	static public String getVersion() {
 		return version;
