dd37887073da7ba39dffbb696e89160aebfdf40d
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-01-02 22:47:27 +0100

    Updated to 0.5.5

--- jtransc-rt-core/src/com/jtransc/JTranscVersion.java
+++ jtransc-rt-core/src/com/jtransc/JTranscVersion.java
@@ -1,7 +1,7 @@
 package com.jtransc;
 
 public class JTranscVersion {
-	static private final String version = "0.5.4";
+	static private final String version = "0.5.5";
 
 	static public String getVersion() {
 		return version;
