943c4f313913ac5e7bf71240a89b480f23e4683a
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-01-07 13:10:58 +0100

    Try to fix MapExt

--- jtransc-utils/src/com/jtransc/lang/MapExt.kt
+++ jtransc-utils/src/com/jtransc/lang/MapExt.kt
@@ -2,8 +2,8 @@ package com.jtransc.lang
 
 import java.util.*
 
-fun <K, V> HashMap<K, V>.putIfAbsentJre7(key: K, value: V): V = if (!containsKey(key)) {
-	put(key, value)!!
+fun <K, V> HashMap<K, V>.putIfAbsentJre7(key: K, value: V): V? = if (!containsKey(key)) {
+	put(key, value)
 } else {
-	get(key)!!
+	get(key)
 }
