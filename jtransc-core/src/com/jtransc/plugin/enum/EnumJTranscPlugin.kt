367dc8dc88f7588c145fdf8eb391081edcceb269
sergei.labutin <sergei@master-code.eu>
2017-03-28 12:43:58 +0300

    fix #94 Support enum with override members

--- jtransc-core/src/com/jtransc/plugin/enum/EnumJTranscPlugin.kt
+++ jtransc-core/src/com/jtransc/plugin/enum/EnumJTranscPlugin.kt
@@ -8,8 +8,9 @@ import com.jtransc.plugin.JTranscPlugin
  * Plugin that include values() method from enums
  */
 class EnumJTranscPlugin : JTranscPlugin() {
+	val enumName = "java.lang.Enum"
 	override fun onTreeShakingAddBasicClass(treeShaking: TreeShakingApi, fqname: FqName, oldclass: AstClass, newclass: AstClass) {
-		if (oldclass.modifiers.isEnum) {
+		if (oldclass.modifiers.isEnum && oldclass.extending.toString() == enumName) {
 			treeShaking.addMethod(AstMethodRef(fqname, "values", AstType.METHOD(ARRAY(oldclass.astType), listOf())), "EnumJTranscPlugin")
 		}
 	}
