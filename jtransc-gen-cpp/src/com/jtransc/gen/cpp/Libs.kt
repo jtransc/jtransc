98b7bf47a830a7f829d8a4ff5693dd45252ed467
intrigus <abc123zeus@live.de>
2017-05-14 00:47:52 +0200

    [CPP-Target] Fix jni headers not being created

--- jtransc-gen-cpp/src/com/jtransc/gen/cpp/Libs.kt
+++ jtransc-gen-cpp/src/com/jtransc/gen/cpp/Libs.kt
@@ -134,15 +134,34 @@ object Libs {
 		val jniDir = File(sdkDir, "jni-headers")
 		if (!jniDir.exists() && !jniDir.mkdir()) throw RuntimeException("Failed")
 		val os = arrayOf("mac", "linux", "win32")
-		File(jniDir, "jni.h").writeBytes(program.resourcesVfs["cpp/jni-headers/jni.h"].readBytes())
-		File(jniDir, "jawt.h").writeBytes(program.resourcesVfs["cpp/jni-headers/jawt.h"].readBytes())
-		File(jniDir, "classfile_constants.h").writeBytes(program.resourcesVfs["cpp/jni-headers/classfile_constants.h"].readBytes())
-		File(jniDir, "jdwpTransport.h").writeBytes(program.resourcesVfs["cpp/jni-headers/jdwpTransport.h"].readBytes())
+		File(jniDir, "jni.h").apply {
+			createNewFile()
+			writeBytes(program.resourcesVfs["cpp/jni-headers/jni.h"].readBytes())
+		}
+
+		File(jniDir, "jawt.h").apply {
+			createNewFile()
+			writeBytes(program.resourcesVfs["cpp/jni-headers/jawt.h"].readBytes())
+		}
+		File(jniDir, "classfile_constants.h").apply {
+			createNewFile()
+			writeBytes(program.resourcesVfs["cpp/jni-headers/classfile_constants.h"].readBytes())
+		}
+		File(jniDir, "jdwpTransport.h").apply {
+			createNewFile()
+			writeBytes(program.resourcesVfs["cpp/jni-headers/jdwpTransport.h"].readBytes())
+		}
 		os.forEach {
 			val osDir = File(jniDir, it)
 			osDir.ensureExists()
-			File(osDir, "jni_md.h").writeBytes(program.resourcesVfs["cpp/jni-headers/${it}/jni_md.h"].readBytes())
-			if (it != "mac") File(osDir, "jawt_md.h").writeBytes(program.resourcesVfs["cpp/jni-headers/${it}/jawt_md.h"].readBytes())
+			File(osDir, "jni_md.h").apply {
+				createNewFile()
+				writeBytes(program.resourcesVfs["cpp/jni-headers/${it}/jni_md.h"].readBytes())
+			}
+			if (it != "mac") File(osDir, "jawt_md.h").apply {
+				createNewFile()
+				writeBytes(program.resourcesVfs["cpp/jni-headers/${it}/jawt_md.h"].readBytes())
+			}
 		}
 	}
 }
