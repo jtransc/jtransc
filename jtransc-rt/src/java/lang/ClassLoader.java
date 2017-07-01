75b785f45404180b097e21c9f2ca9a4d83b6331e
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-07-01 21:25:23 +0200

    [CPP] Removed ClassLoader C++ specialized code. Since this is not going to work at least with the current reflection library.

--- jtransc-rt/src/java/lang/ClassLoader.java
+++ jtransc-rt/src/java/lang/ClassLoader.java
@@ -190,12 +190,12 @@ public abstract class ClassLoader {
 		}
 	}
 
-	@JTranscMethodBody(target = "cpp", value = "return ptr_to_jlong(N::jtvmLoadDynamicLibraray(N::istr3(p0).c_str()));")
+	//@JTranscMethodBody(target = "cpp", value = "return ptr_to_jlong(N::jtvmLoadDynamicLibraray(N::istr3(p0).c_str()));")
 	private static long loadLibrarayCpp(String filename) {
 		return 0L;
 	}
 
-	@JTranscMethodBody(target = "cpp", value = "N::jtvmUnLoadDynamicLibraray(jlong_to_ptr(p0));")
+	//@JTranscMethodBody(target = "cpp", value = "N::jtvmUnLoadDynamicLibraray(jlong_to_ptr(p0));")
 	private static void unLoadLibrarayCpp(long handle) {
 	}
 }
