364d7601c5321bb2ccca4c4e5e0e7732b369c13c
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-07-10 21:58:19 +0200

    Started plugin + sample

---
+++ jtransc-core/src/com/jtransc/plugin/junit/JUnitJTranscPlugin.kt
@@ -0,0 +1,23 @@
+package com.jtransc.plugin.junit
+
+class JUnitJTranscPlugin {
+
+}
+
+/*
+public class Demo {
+    static public void main(String[] args) {
+        JUnitCore core = new JUnitCore();
+        core.addListener(new TextListener(System.out));
+        core.run(Test1.class);
+    }
+}
+
+public class Test1 {
+    @Test
+    @JTranscKeep // @Test should work like @JTranscKeep
+    public void test1() {
+        Assert.assertEquals(1, 0);
+    }
+}
+*/
