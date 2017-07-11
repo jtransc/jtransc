5198e966907c932bbbe2f8562e5745419206b799
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-07-11 08:28:37 +0200

    Initial work on plugin

--- jtransc-core/src/com/jtransc/plugin/junit/JUnitJTranscPlugin.kt
+++ jtransc-core/src/com/jtransc/plugin/junit/JUnitJTranscPlugin.kt
@@ -1,10 +1,31 @@
 package com.jtransc.plugin.junit
 
-class JUnitJTranscPlugin {
+import com.jtransc.ast.*
+import com.jtransc.plugin.JTranscPlugin
+import com.jtransc.plugin.reflection.createClass
+import com.jtransc.plugin.reflection.createMethod
+import com.sun.org.apache.bcel.internal.generic.NEW
 
+class JUnitJTranscPlugin : JTranscPlugin() {
+	fun createEntry(program: AstProgram, classesToTest: Iterable<FqName>) {
+		val JunitEntryPointClass = program.createClass("JunitEntryPoint".fqname) {
+			createMethod(
+				name = "main",
+				desc = AstType.METHOD(AstType.VOID, listOf(AstType.ARRAY(AstType.STRING))),
+				isStatic = true
+			) {
+				val coreLocal = AstLocal(0, AstType.REF("org.junit.runner.JUnitCore".fqname))
+				SET(coreLocal, AstExpr.NEW_WITH_CONSTRUCTOR(AstMethodRef("org.junit.runner.JUnitCore".fqname, "<init>", AstType.METHOD(AstType.VOID, listOf())), listOf()))
+				//STM(AstExpr.CALL_INSTANCE(coreLocal.expr, AstMethodRef))
+			}
+		}
+	}
 }
 
 /*
+import org.junit.internal.TextListener;
+import org.junit.runner.JUnitCore;
+
 public class Demo {
     static public void main(String[] args) {
         JUnitCore core = new JUnitCore();
