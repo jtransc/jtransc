c18fe196a78fea4d2527d00957a7b0adc0f09ed0
Sergey Labutin <sergei@master-code.eu>
2017-07-21 21:58:30 +0300

    ignore for master

--- jtransc-main/test/HaxeTest.kt
+++ jtransc-main/test/HaxeTest.kt
@@ -120,5 +120,6 @@ class HaxeTest : _Base() {
 	@Ignore
 	@Test fun testHaxeStringBuilderTestIssue138() = testClass(Params(clazz = HaxeStringBuilderTestIssue138::class.java, minimize = false, log = false, lang = "cpp", debug = true))
 
+	@Ignore
 	@Test fun testIssue219() = testClass(Params(clazz = Issue219::class.java, minimize = false, log = false, lang = "cpp", debug = true, treeShaking = true))
 }
