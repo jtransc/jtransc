b8ccde63049daab9c2a740f991d52fc55592495a
sergey.labutin <sergei@master-code.eu>
2017-08-02 16:41:26 +0300

    Fix thread mapping

--- jtransc-rt-core/src/com/jtransc/thread/JTranscThreading.java
+++ jtransc-rt-core/src/com/jtransc/thread/JTranscThreading.java
@@ -3,11 +3,13 @@ package com.jtransc.thread;
 import com.jtransc.JTranscSystem;
 import com.jtransc.annotation.JTranscMethodBody;
 import com.jtransc.annotation.JTranscMethodBodyList;
+import com.jtransc.annotation.haxe.HaxeAddMembers;
 import com.jtransc.annotation.haxe.HaxeMethodBody;
 
 public class JTranscThreading {
 	static public Impl impl = new Impl(null);
 
+	@HaxeAddMembers("private static var threadsMap = new haxe.ds.ObjectMap<Dynamic, {% CLASS java.lang.Thread %}>();")
 	static public class Impl {
 		Impl parent;
 
@@ -19,8 +21,18 @@ public class JTranscThreading {
 			if (parent != null) return parent.isSupported();
 			return _isSupported();
 		}
+		@HaxeMethodBody(target = "cpp", value = "return threadsMap.get(cpp.vm.Thread.current().handle);")
+		public Thread getCurrent() {
+			return null;
+		}
 
-		@HaxeMethodBody(target = "cpp", value = "cpp.vm.Thread.create(function():Void{p0{% IMETHOD java.lang.Runnable:run %}();});")
+		@HaxeMethodBody(target = "cpp", value =
+			"cpp.vm.Thread.create(function():Void{" +
+				"var h = cpp.vm.Thread.current().handle;" +
+				"threadsMap.set(h, p0);" +
+				"p0{% IMETHOD java.lang.Runnable:run %}();" +
+				"threadsMap.remove(h);" +
+			"});")
 		public void start(Thread thread) {
 			if (parent != null) {
 				parent.start(thread);
