ff08b18db6ea3e8394135b71fa6830bbc53da824
sergey.labutin <sergei@master-code.eu>
2017-08-12 19:15:20 +0300

    ignore for test

--- jtransc-gen-haxe/test/HaxeTest.kt
+++ jtransc-gen-haxe/test/HaxeTest.kt
@@ -121,5 +121,6 @@ class HaxeTest : _Base() {
 	@Ignore
 	@Test fun testHaxeStringBuilderTestIssue138() = testClass(Params(clazz = HaxeStringBuilderTestIssue138::class.java, minimize = false, log = false, lang = "cpp", debug = true))
 
+	@Ignore
 	@Test fun testJTranscBug244() = testClass(Params(clazz = JTranscBug244::class.java, minimize = false, log = false, lang = "cpp", debug = true))
 }
