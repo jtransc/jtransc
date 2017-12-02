f9a2cd8419def7948be45b71085b292bc9a96933
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-12-02 16:35:50 +0100

    Kotlin 1.2 + Gradle 4.3.1 (#282)
    * Kotlin to 1.2
    
    * Missing `./gradlew updateVersion` + gradle 4.3.1

--- jtransc-rt-core/src/com/jtransc/JTranscVersion.java
+++ jtransc-rt-core/src/com/jtransc/JTranscVersion.java
@@ -3,7 +3,7 @@ package com.jtransc;
 import com.jtransc.annotation.JTranscSync;
 
 public class JTranscVersion {
-	static private final String version = "0.6.7";
+	static private final String version = "0.6.8";
 
 	@JTranscSync
 	static public String getVersion() {
